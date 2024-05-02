f4b210dd39e86d51b46cc216ab4ca139a9ce1bb2
Malihe Ghaderi <malihe.ghaderi@snapp.cab>
2024-05-02 11:48:45 +0330

    feat: add button and input components

---
+++ src/components/common/button/button.tsx
@@ -0,0 +1,11 @@
+import styles from './button.module.scss';
+
+const Button = ({ children, ...props }) => {
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
